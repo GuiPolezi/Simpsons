c8f0f0dd22da055cb784d53143f48684691beea5
Guilherme <gui.polezi@hotmail.com>
2022-07-27 18:16:35 -0300

    Update game.js

--- js/game.js
+++ js/game.js
@@ -94,21 +94,21 @@ const revealCard = ({ target }) => {
     }  
 }
 
-const createcard = (character) => {
+const createCard = (character) => {
 
-    const card = createElement('div', 'card');
-    const front = createElement('div', 'face front');
-    const back = createElement('div', 'face back');
-  
-    front.style.backgroundImage = `url('../images/${character}.png')`;
-  
-    card.appendChild(front);
-    card.appendChild(back);
-  
-    card.addEventListener('click', revealCard);
-    card.setAttribute('data-character', character)
+  const card = createElement('div', 'card');
+  const front = createElement('div', 'face front');
+  const back = createElement('div', 'face back');
 
-    return card;
+  front.style.backgroundImage = `url('../images/${character}.png')`;
+
+  card.appendChild(front);
+  card.appendChild(back);
+
+  card.addEventListener('click', revealCard);
+  card.setAttribute('data-character', character)
+
+  return card;
 }
 
   const loadgame = () => {
